f700c4d5d9c6ec37fecca87b1ca07bf814fa80d9
Pamelita123 <pame.ado3780@gmail.com>
2024-08-11 12:35:01 -0600

    abc

--- class/database.php
+++ class/database.php
@@ -7,7 +7,7 @@ class database{
     // paramentros que le voy a enviar al objeto pdo
     private $PDOlocal;
     private $user = 'root';
-    private $password = "inicio";
+    private $password = "";
     private $server = "mysql:host=localhost;dbname=glass_store_ana";
 
     // le ponemos la sig cadena: host, base de datos
